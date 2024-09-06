8c36296ef8bf9ea08ba4cfdda3ea955e22aabb91
icambraa <126367844+icambraa@users.noreply.github.com>
2024-09-06 02:42:08 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Dashboard from './pages/Dashboard';
 import PasswordProtectedLink from './pages/PasswordProtectedLink';
-import ContactPage from './pages/ContactPage'; // Import the new ContactPage
+import ContactPage from './pages/ContactPage'; 
 import { AuthProvider } from './context/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route
@@ -22,9 +21,9 @@ function App() {
             }
           />
           <Route path="/password-protected/:shortUrl" element={<PasswordProtectedLink />} />
-          <Route path="/contact" element={<ContactPage />} /> {/* Add this new route */}
+          <Route path="/contact" element={<ContactPage />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </AuthProvider>
   );
 }
